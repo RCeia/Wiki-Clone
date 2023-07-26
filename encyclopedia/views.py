bbf38d33c61d169d581211cef6e413aab7d1f096
Ceia <rodrigoceia7337@gmail.com>
2023-07-26 14:55:02 +0100

    Add Search Bar as a Form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
+class SearchForm(forms.Form):
+    # Form Class For Search Bar
+    search = forms.CharField(label='', widget=forms.TextInput(attrs={
+      "class": "search",
+      "placeholder": "Search Zikipedia"}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": SearchForm(),
     })
 
